733be509c32d3f47eaf2e249f961fedcafae98db
chx <1369185461@qq.com>
2021-11-25 10:05:59 +0800

    admin模块联通数据库

--- yunyi-admin/src/main/java/com/cy/yunyi/admin/controller/UmsAdminController.java
+++ yunyi-admin/src/main/java/com/cy/yunyi/admin/controller/UmsAdminController.java
@@ -2,8 +2,10 @@ package com.cy.yunyi.admin.controller;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.crypto.digest.BCrypt;
+import com.cy.yunyi.admin.service.AdminService;
 import com.cy.yunyi.common.domain.UserDto;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -15,11 +17,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin")
 public class UmsAdminController {
 
+    @Autowired
+    private AdminService adminService;
+
     @ApiOperation("根据用户名获取通用用户信息")
     @GetMapping("/loadByUsername")
     public UserDto loadUserByUsername(@RequestParam String username) {
-//        UserDto userDTO = adminService.loadUserByUsername(username);
-//        return userDTO;
-        return new UserDto(1L,username, BCrypt.hashpw("123456"),1, "client-app", CollUtil.toList("ADMIN","TEST"));
+        UserDto userDTO = adminService.loadUserByUsername(username);
+        return userDTO;
+//        return new UserDto(1L,username, BCrypt.hashpw("123456"),1, "client-app", CollUtil.toList("ADMIN","TEST"));
     }
 }
