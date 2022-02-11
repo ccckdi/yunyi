756dcba3c6dac89a74d921e0f98bd3451830d8bf
caihaixin <caihaixin@dataexa.com>
2022-02-11 18:55:41 +0800

    添加商品推荐模块

---
+++ yunyi-portal/src/main/java/com/cy/portal/recommend/CalculateItemScore.java
@@ -0,0 +1,26 @@
+package com.cy.portal.recommend;
+
+/**
+ * @Author caihx
+ * @Description: 根据用户浏览、收藏、购买、计算物品得分
+ * @Date 2022/2/11
+ */
+public class CalculateItemScore {
+
+    /**
+     * 浏览 1分
+     * 收藏 2分
+     * 购买 3分
+     */
+    public void computeResultByUserId(){
+        //读取用户浏览数据
+        //读取用户收藏数据
+        //读取用户订单数据
+    }
+
+    public void InitComputeResult(){
+        //读取用户浏览数据
+        //读取用户收藏数据
+        //读取用户订单数据
+    }
+}
---
+++ yunyi-portal/src/main/java/com/cy/portal/recommend/UserBasedRecommend.java
@@ -0,0 +1,9 @@
+package com.cy.portal.recommend;
+
+/**
+ * @Author caihx
+ * @Description: 基于用户相似度的推荐引擎
+ * @Date 2022/2/11
+ */
+public class UserBasedRecommend {
+}
