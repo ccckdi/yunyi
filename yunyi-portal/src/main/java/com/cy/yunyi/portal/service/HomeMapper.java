61285074539dbee45432d11aa35d9f2339da81fd
chx <1369185461@qq.com>
2022-04-24 22:55:13 +0800

    修改依赖关系

---
+++ yunyi-portal/src/main/java/com/cy/yunyi/portal/service/HomeMapper.java
@@ -0,0 +1,12 @@
+package com.cy.yunyi.portal.service;
+
+import com.cy.yunyi.portal.vo.HomeContentVo;
+
+/**
+ * @Author: chx
+ * @Description: TODO
+ * @DateTime: 2022/4/24 22:41
+ **/
+public interface HomeMapper {
+    HomeContentVo content();
+}
